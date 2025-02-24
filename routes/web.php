6ac8662bbb9e514480b0e21ad10181f47cb26625
Abderrahmane Rabeh <rabehlife144@gmail.com>
2025-02-24 16:44:17 +0000

    Ajouter diagram de class et useCase

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+|   
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
@@ -28,4 +28,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
